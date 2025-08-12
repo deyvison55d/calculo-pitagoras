1d948759f8b7c7afa740337451e754f150f5a30c
Deyvison <deyvisonsousa74@gmail.com>
2025-08-12 13:49:34 -0300

    Update teste.py

--- projeto/teste.py
+++ projeto/teste.py
@@ -1,23 +1,25 @@
 from menu import *
 from funções import *
 
+while True:
 # Exibe o menu e solicita a opção do usuário
-res = menu('Calculo de Pitágoras')
+    res = menu('Calculo de Pitágoras')
 
-if res == 1: # Calcular cateto oposto
-    resultado = cateto_oposto()
-    print(f'O cateto oposto é: {resultado:.2f}')
-
-
-elif res == 2: # Calcular cateto adjacente
-    resultado = cateto_adjacente()
-    print(f'O cateto adjacente é: {resultado:.2f}')
+    if res == 1: # Calcular cateto oposto
+        resultado = cateto_oposto()
+        print(f'O cateto oposto é: {resultado:.2f}')
 
 
-elif res == 3: # Calcular hipotenusa
-    resultado = hipotenusa()
-    print(f'A hipotenusa é: {resultado:.2f}')
+    elif res == 2: # Calcular cateto adjacente
+        resultado = cateto_adjacente()
+        print(f'O cateto adjacente é: {resultado:.2f}')
 
 
-elif res == 4: #Sair
-    print('Saindo do programa... Até logo!')
+    elif res == 3: # Calcular hipotenusa
+        resultado = hipotenusa()
+        print(f'A hipotenusa é: {resultado:.2f}')
+
+
+    elif res == 4: #Sair
+        print('Saindo do programa... Até logo!')
+        break
